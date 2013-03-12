f9bc036ec978e51ba420f0c836fc034f5884aed0
Joseph Nudell <joenudell@gmail.com>
2013-03-12 05:04:21 -0500

    initial commit of controllers

---
+++ web/fetchbracket.py
@@ -0,0 +1,78 @@
+#!/var/chroot/home/content/41/10223541/html/python/bin/python
+# -*- coding: utf8 -*-
+'''
+$ python ncaa2013/web/fetchbracket.py
+
+MVC-Controller for retrieving brackets from ncaa.db. Returns JSON.
+
+Copyright (c) 2013 Joe Nudell
+Freely distributable under the MIT License.
+'''
+
+import cgi
+import json
+import os
+from ncaa import *
+
+
+# Get GET
+args = cgi.FieldStorage()
+
+db = args.getvalue('db', True)
+method = args.getvalue('method', 'nested')
+key = args.getvalue('id', '-1')
+filter_ = args.getvalue('f', 'id').lower()
+
+ret = dict()
+
+if key is None:
+    ret['error'] = 'key not specified'
+
+else:
+    if db=='1' or db=='true':
+        # Fetch from db
+        session = load_db('../../data/ncaa.db')
+        
+        try:
+            # Try to pull tournament record from database
+            t = None
+            
+            if filter_=='id':
+                # Default: pull by ID
+                t = session.query(Tournament).get(int(key))
+            elif filter_=='season':
+                # Alternative: pull by season
+                t = session.query(Tournament).filter_by(season=key).one()
+            else:
+                # No other query filters are supported currently
+                ret['error'] = 'unsupported query filter'
+                
+            if t is not None:
+                ret = t.export(method)
+        except:
+            ret['error'] = 'key not found'
+
+    else:
+        # Fetch from local scrap
+        spaths = [key+'.js', key+'.json',
+                  'brackets/'+key+'.js', 'brackets/'+key+'.json']
+        for path in spaths:
+            if os.path.exists(path):
+                with open(path, 'r') as fh:
+                    t = json.loads(fh.read())
+                    ret['nodes'] = t
+                break
+            else:
+                ret['error'] = 'record not found'
+
+
+
+#### RETURN #####
+print "Content-type: application/json;charset=utf-8"
+print
+## Content ##
+if type(ret) is dict:
+    print json.dumps(ret)
+else:
+    # Already JSON-ified
+    print ret
---
+++ web/fetchstats.py
@@ -0,0 +1,68 @@
+#!/var/chroot/home/content/41/10223541/html/python/bin/python
+# -*- coding: utf8 -*-
+'''
+$ python ncaa2013/web/fetchstats.py
+
+MVC-Controller for retrieving stats (by ID) from ncaa.db. Returns JSON.
+
+Copyright (c) 2013 Joe Nudell
+Freely distributable under the MIT License.
+'''
+
+import cgi
+import json
+import os
+from ncaa import *
+
+
+# Get GET
+args = cgi.FieldStorage()
+
+type = args.getvalue('type', None)
+id_ = args.getvalue('id', None)
+er = False
+
+try:
+    id_ = int(id_)
+except:
+    ret['error'] = 'bad ID'
+    er = True
+
+ret = dict()
+
+if type is None:
+    ret['error'] = 'type not specified'
+
+elif id is None:
+    ret['error'] = 'id not specified'
+
+elif not er:
+    # Connect to DB
+    session = load_db('../../data/ncaa.db')
+    
+    # Get entity from DB.
+    if type.lower() == 'squad':
+        e = session.query(Squad).get(id_)
+        
+        # Place general info about Team in return dict
+        ret['name'] = e.team.name
+        ret['season'] = e.season
+        ret['wins'] = len(e.wins)
+        ret['losses'] = len(e.losses)
+        
+        # Place stats object in return dict
+        ret['stats'] = dict(e.stats.items())
+        ret['stats']['rpi'] = e.rpi
+        ret['stats']['wwp'] = e.win_pct(weighted=True)
+
+    else:
+        # Nothing else supported so far
+        ret['error'] = 'type not currently supported'
+
+
+## RETURN ##
+## HEADER ##
+print "Content-type: application/json;charset=utf-8"
+print
+## CONTENT ##
+print json.dumps(ret)
