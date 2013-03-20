5efea76d619db105b09194a8a03a3783a47af924
Joseph Nudell <joenudell@gmail.com>
2013-03-19 22:18:05 -0500

    formerly grid_search. moved back to recommending sklearn.grid_search.GridSearchCV (must be 0.13+ though!)

--- ncaalib/eval.py
+++ ncaalib/eval.py
@@ -1,26 +1,11 @@
 #-*- coding: utf8 -*-
 '''
-ncaalib.grid_search
+ncaalib.eval
 
 Custom scoring object for scikit-learn model evaluation.
 
-Parts that I wrote:
 Copyright (c) 2013 Joseph Nudell
 Freely distributable under the MIT license.
-
-The GridSearch class and fit_grid_point function are hacky modifications
-of sklearn.grid_search.GridSearchCV and sklearn.grid_search.fit_grid_point.
-The attribution of that code is:
-
-# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>,
-#         Gael Varoquaux <gael.varoquaux@normalesup.org>
-# License: BSD Style.
-
-I took it from
-
-https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/grid_search.py
-
-on March 17. 2013.
 '''
 __author__="Joseph Nudell"
 __date__="March 18, 2013"
@@ -28,13 +13,6 @@ __date__="March 18, 2013"
 
 from ncaa import *
 from aux.output import print_warning
-try:
-    from sklearn.grid_search import ParameterGrid
-except ImportError:
-    from sklearn.grid_search import IterGrid as ParameterGrid
-from sklearn.utils.validation import _num_samples, check_arrays
-from sklearn.externals.joblib import Parallel, delayed, logger
-from sklearn.base import clone
 import copy
 import numbers
 import time
@@ -171,196 +149,6 @@ class ExtractedTournament(object):
 
         return self.score()
 
-
-
-
-
-
-
-
-
-
-def fit_grid_point(X, y, base_clf, clf_params, scorer,
-                   verbose, loss_func=None, **fit_params):
-    '''Modified version of sklearn.grid_search.fit_grid_point that trains
-    on entire input set. Note that if test set is not ignored by scorer,
-    the resulting accuracy will be meaningless (you will have fit the data
-    to the test set). The scorer should have its own testing routine that
-    does not use data provided as inputs here.
-    
-    TODO: clean up this function. Right now it's just a hack of the original.
-    '''
-    if verbose > 1:
-        start_time = time.time()
-        msg = '%s' % (', '.join('%s=%s' % (k, v)
-                      for k, v in clf_params.items()))
-        print("[GridSearch] %s %s" % (msg, (64 - len(msg)) * '.'))
-
-    # update parameters of the classifier after a copy of its base structure
-    clf = clone(base_clf)
-    clf.set_params(**clf_params)
-
-    if hasattr(base_clf, 'kernel') and callable(base_clf.kernel):
-        # cannot compute the kernel values with custom function
-        raise ValueError("Cannot use a custom kernel function. "
-                         "Precompute the kernel matrix instead.")
-
-    if not hasattr(X, "shape"):
-        if getattr(base_clf, "_pairwise", False):
-            raise ValueError("Precomputed kernels or affinity matrices have "
-                             "to be passed as arrays or sparse matrices.")
-        X_train = X
-        X_test = X
-    else:
-        if getattr(base_clf, "_pairwise", False):
-            # X is a precomputed square kernel matrix
-            if X.shape[0] != X.shape[1]:
-                raise ValueError("X should be a square kernel matrix")
-            X_train = X
-            X_test = X
-        else:
-            X_train = X
-            X_test = X
-
-    if y is not None:
-        y_test = y
-        y_train = y
-        clf.fit(X_train, y_train, **fit_params)
-
-        if scorer is not None:
-            this_score = scorer(clf, X_test, y_test)
-        else:
-            this_score = clf.score(X_test, y_test)
-    else:
-        clf.fit(X_train, **fit_params)
-        if scorer is not None:
-            this_score = scorer(clf, X_test)
-        else:
-            this_score = clf.score(X_test)
-
-    if not isinstance(this_score, numbers.Number):
-        raise ValueError("scoring must return a number, got %s (%s)"
-                         " instead." % (str(this_score), type(this_score)))
-
-    if verbose > 2:
-        msg += ", score=%f" % this_score
-    if verbose > 1:
-        end_msg = "%s -%s" % (msg,
-                              logger.short_format_time(time.time() -
-                                                       start_time))
-        print("[GridSearch] %s %s" % ((64 - len(end_msg)) * '.', end_msg))
-    return this_score, clf_params, _num_samples(X_test)
-
-
-
-
-
-
-
-
-class GridSearch(object):
-    '''Perform a grid search with specified grid, searching for highest score
-    on specified scorer. Adapted from GridSearchCV, but does no folding.
-    It makes sense to use this when optimizing on a fixed testing set that is
-    not in an array format, such as the Tournament simulations.'''
-    def __init__(self, classifier, grid,
-                       seasons=['2009-10', '2010-11', '2011-12'], verbose=1,
-                       refit=False, pre_dispatch='2*n_jobs',
-                       n_jobs=1, scoring=None, fit_params=None, **kwargs):
-        self.verbose = verbose
-        self.refit = refit
-        self.seasons = seasons
-        self.classifier = classifier
-        self.grid = ParameterGrid(grid)
-        self.pre_dispatch = pre_dispatch
-        self.n_jobs = n_jobs
-        if scoring is None:
-            raise ArgumentError("You have to specify `scoring` parameter")
-        self.scoring = scoring
-        self.fit_params = fit_params if fit_params is not None else {}
-    
-    def _set_methods(self):
-        '''Create predict and predict_proba if present in best estimator.'''
-        if hasattr(self.best_estimator_, 'predict'):
-            self.predict = self.best_estimator_.predict
-        if hasattr(self.best_estimator_, 'predict_proba'):
-            self.predict_proba = self.best_estimator_.predict_proba
-
-    def fit(self, X, y):
-        '''Run the grid search.
-        
-        TODO: Clean up this method. Right now it's an ugly hack of the
-        original that removes CV.
-        '''
-        grid = self.grid
-        base_clf = clone(self.classifier)
-        scorer = self.scoring
-        
-        n_samples = _num_samples(X)
-        X, y = check_arrays(X, y, allow_lists=True, sparse_format='csr')
-
-        pre_dispatch = self.pre_dispatch
-
-        # Dispatch grid search
-        out = Parallel(
-            n_jobs=self.n_jobs, verbose=self.verbose,
-            pre_dispatch=pre_dispatch)(
-                delayed(fit_grid_point)(
-                    X, y, base_clf, clf_params, scorer,
-                    self.verbose, **self.fit_params) for clf_params in grid)
-
-        # Out is a list of triplet: score, estimator, n_test_samples
-        n_param_points = len(list(grid))
-        n_fits = len(out)
-
-        scores = list()
-
-        for grid_start in range(0, n_fits):
-            n_test_samples = 0
-            score = 0
-            these_points = list()
-            
-            for this_score, clf_params, this_n_test_samples in \
-                    out[grid_start:grid_start + 1]:
-                these_points.append(this_score)
-                score += this_score
-                
-            scores.append((score, clf_params))
-
-        # Get comparison direction
-        if scorer is not None:
-            greater_is_better = scorer.greater_is_better
-        else:
-            greater_is_better = True
-
-        if greater_is_better:
-            best_score = -np.inf
-        else:
-            best_score = np.inf
-
-        for score, params in scores:
-            if ((score > best_score and greater_is_better)
-                    or (score < best_score and not greater_is_better)):
-                best_score = score
-                best_params = params
-
-        self.best_params_ = best_params
-        self.best_score_ = best_score
-
-        if self.refit:
-            # fit the best estimator using the entire dataset
-            # clone first to work around broken estimators
-            best_estimator = clone(base_clf).set_params(**best_params)
-            if y is not None:
-                best_estimator.fit(X, y, **self.fit_params)
-            else:
-                best_estimator.fit(X, **self.fit_params)
-            self.best_estimator_ = best_estimator
-            self._set_methods()
-
-        return self
-
-        
 
 
 
